9485fc9286d76a5747de45e8cafd865e04e6073e
MohammadKardi1 <mohammadkardi1@gmail.com>
2024-10-29 18:19:44 +0100

    add syntax for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -26,11 +26,29 @@ const navLinks = [
 
 const Header = () => {
 
-  
+  const headerRef = useRef(null)
+  const menuRef = useRef(null)
 
+  const handleStickyHeader = () => {
+    window.addEventListener('scroll', () => {
+      if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        headerRef.current.classList.add('sticky__header')
+      } else {
+        headerRef.current.classList.remove('sticky__header')
+      }
+    })
+  }
+
+  useEffect(() => {
+    handleStickyHeader()
+
+    return () => window.removeEventListener('scroll', handleStickyHeader)
+  })
+
+  const toggleMenu = () => menuRef.current.classList.toggle('show__menu')
 
   return (
-    <header className='header flex items-center'>
+    <header className='header flex items-center' ref={headerRef}>
       <div className='container'>
         <div className='flex items-center justify-between'>
 
@@ -41,7 +59,7 @@ const Header = () => {
 
 
           {/* ========= menu ========= */}
-          <div className='navigation'>
+          <div className='navigation' ref={menuRef} onClick={toggleMenu}>
             <ul className='menu flex items-center gap-[2.7rem]'>
               {
                 navLinks.map((link, index) => (
@@ -78,14 +96,14 @@ const Header = () => {
                 </button>
               </Link>
 
-              <span className='md:hidden'>
+              <span className='md:hidden' onClick={toggleMenu}>
                 <BiMenu className='w-6 h-6 cursor-pointer'/>
               </span>
 
 
-
-
             </div>
+
+
 
           
 
