95ca666e8293a94864ad1429c642a33f41b09ef0
MohammadKardi1 <mohammadkardi1@gmail.com>
2024-11-01 16:55:43 +0100

    update service card file

--- src/pages/Home/ServicesSection/ServiceCard.jsx
+++ src/pages/Home/ServicesSection/ServiceCard.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import { BsArrowRight } from "react-icons/bs";
 import { Link } from 'react-router-dom';
 
-const ServiceCard = ({index, item}) => {
-
+const ServiceCard = ({item, index}) => {
 
     const {name, desc, bgColor, textColor} = item
 
   return (
-    <div key={index} className='py-[30px] px-3 lg:px-5'>
+    <div className='py-[30px] px-3 lg:px-5'>
         <h1 className='text-[26px] leading-9 text-headingColor font-[700]'>
             {name}
         </h1>
@@ -22,8 +21,7 @@ const ServiceCard = ({index, item}) => {
                 items-center justify-center group hover:bg-primaryColor hover:border-none' >
                 <BsArrowRight className='group-hover:text-white w-6 h-5'/>
             </Link>
-            <span 
-                className='w-[44px] h-[44px] flex items-center justify-center text-[18px] leading-[30px] font-[600]'
+            <span className='w-[44px] h-[44px] flex items-center justify-center text-[18px] leading-[30px] font-[600]'
                 style={{background: `${bgColor}`, color: `${textColor}`, borderRadius: "6px 0 0 6px"}}>
                 {index + 1}
             </span>
